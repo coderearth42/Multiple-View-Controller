b1c1ac957c3b913d57282fc7c5caeb22486916fa
vinoth <vinothvino42@gmail.com>
2017-05-15 02:28:29 +0530

    first commit

---
+++ MultipleViewControllersExample/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  MultipleViewControllersExample
+//
+//  Created by Vinoth Vino on 15/05/17.
+//  Copyright © 2017 CoderEarth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Second View Controller")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MultipleViewControllersExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MultipleViewControllersExample
+//
+//  Created by Vinoth Vino on 15/05/17.
+//  Copyright © 2017 CoderEarth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("First View Controller")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
